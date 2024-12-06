c22a532f903a4a2a2ec1bc7547ecab179a08bf4d
Rafael Peralta <rafael12peralta@gmail.com>
2024-12-06 17:04:23 -0400

    small changes

--- quiz_app/lib/controllers/quiz_controller.dart
+++ quiz_app/lib/controllers/quiz_controller.dart
@@ -3,31 +3,36 @@ import 'package:flutter/material.dart';
 import '../models/quiz_question.dart';
 
 final questions = [
+  Quiz(question: "Who discovered the Law of Gravity?", answers: [
+    "Sir Isaac Newton",
+    "Galileo Galilei",
+    "Charles Darwin",
+    "Albert Einstein"
+  ]),
+  Quiz(question: "What does DNA stand for?", answers: [
+    "Deoxyribonucleic Acid",
+    "Deoxyribogenetic Acid",
+    "Deoxyribogenetic Atoms",
+    "Detoxic Acid"
+  ]),
   Quiz(
-      question: "What animation studio produced &quot;Gurren Lagann&quot;?",
-      answers: ["Gainax", "Kyoto Animation", "Pierrot", "A-1 Pictures"]),
+      question: "Which element has the highest melting point?",
+      answers: ["Carbon", "Tungsten", "Platinum", "Osmium"]),
   Quiz(
-      question: "What animation studio produced &quot;Gurren Lagann&quot;?",
-      answers: ["Gainax", "Kyoto Animation", "Pierrot", "A-1 Pictures"]),
+      question: "What is the unit of electrical resistance?",
+      answers: ["Ohm", "Mho", "Tesla", "Joule"]),
   Quiz(
-      question: "What animation studio produced &quot;Gurren Lagann&quot;?",
-      answers: ["Gainax", "Kyoto Animation", "Pierrot", "A-1 Pictures"]),
+      question: "What is the powerhouse of the cell?",
+      answers: ["Mitochondria", "Ribosome", "Redbull", "Nucleus"]),
   Quiz(
-      question: "What animation studio produced &quot;Gurren Lagann&quot;?",
-      answers: ["Gainax", "Kyoto Animation", "Pierrot", "A-1 Pictures"]),
+      question: "The asteroid belt is located between which two planets?",
+      answers: [
+        "Mars and Jupiter",
+        "Jupiter and Saturn",
+        "Mercury and Venus",
+        "Earth and Mars"
+      ]),
   Quiz(
-      question: "What animation studio produced &quot;Gurren Lagann&quot;?",
-      answers: ["Gainax", "Kyoto Animation", "Pierrot", "A-1 Pictures"]),
-  Quiz(
-      question: "What animation studio produced &quot;Gurren Lagann&quot;?",
-      answers: ["Gainax", "Kyoto Animation", "Pierrot", "A-1 Pictures"]),
-  Quiz(
-      question: "What animation studio produced &quot;Gurren Lagann&quot;?",
-      answers: ["Gainax", "Kyoto Animation", "Pierrot", "A-1 Pictures"]),
-  Quiz(
-      question: "What animation studio produced &quot;Gurren Lagann&quot;?",
-      answers: ["Gainax", "Kyoto Animation", "Pierrot", "A-1 Pictures"]),
-  Quiz(
-      question: "What animation studio produced &quot;Gurren Lagann&quot;?",
-      answers: ["Gainax", "Kyoto Animation", "Pierrot", "A-1 Pictures"]),
+      question: "How many planets are in our Solar System?",
+      answers: ["Eight", "Nine", "Seven", "Ten"]),
 ];
